4b6977538dfa1b1dad67bb8ec72213d9add65345
Sanjai Kumar Mani <sanjaikumar.mani@gmail.com>
2021-12-15 23:16:34 +0400

    added stuff

---
+++ src/modules/layout.js
@@ -0,0 +1,14 @@
+/* eslint-disable class-methods-use-this */
+function basicLayout() {
+
+}
+class layout {
+  constructor() {
+    this.tempArray = ['helow'];
+  }
+
+  start() {
+    basicLayout();
+  }
+}
+export default layout;
