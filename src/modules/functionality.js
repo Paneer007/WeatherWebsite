dff76ad3c9e6c49c9d93ae5991c149d572381120
Sanjai Kumar Mani <sanjaikumar.mani@gmail.com>
2021-12-16 13:54:43 +0400

    Added stuff

--- src/modules/functionality.js
+++ src/modules/functionality.js
@@ -18,7 +18,7 @@ async function fetchDataFromWeatherSite(url) {
 }
 function SearchForCity() {
   const SearchBar = document.getElementById('SearchInput');
-  const APIRequestURL = `http://api.openweathermap.org/data/2.5/weather?q=${SearchBar.value}&APPID=1a39cb86f43df320f7a215022e13f0b7`;
+  const APIRequestURL = `https://api.openweathermap.org/data/2.5/weather?q=${SearchBar.value}&APPID=1a39cb86f43df320f7a215022e13f0b7`;
   fetchDataFromWeatherSite(APIRequestURL);
 }
 class functionality {
